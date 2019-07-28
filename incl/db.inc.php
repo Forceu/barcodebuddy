c4c1c18c3a0280feb9a710898499342f265ed437
Forceu <Marc-Ole@gmx.de>
2019-07-28 11:32:35 +0200

    Fixed major Problem with DB communication
    Bug prevented Barcode Buddy to run properly

--- incl/db.inc.php
+++ incl/db.inc.php
@@ -37,8 +37,7 @@ function initDb() {
     $db->exec("CREATE TABLE IF NOT EXISTS BarcodeLogs(id INTEGER PRIMARY KEY, log TEXT NOT NULL)");
     $db->exec("INSERT INTO TransactionState(id,currentState,since) SELECT 1, 0, datetime('now','localtime') WHERE NOT EXISTS(SELECT 1 FROM TransactionState WHERE id = 1)");
     $db->exec("CREATE TABLE IF NOT EXISTS BBConfig(id INTEGER PRIMARY KEY, data TEXT NOT NULL, value TEXT NOT NULL)");
-    $db->exec("INSERT INTO BBConfig(id,data,value) SELECT 1, \"version\", \"" . BB_VERSION . "\" WHERE NOT EXISTS(SELECT 1 FROM BBLogs WHERE id = 1)");
-    
+    $db->exec("INSERT INTO BBConfig(id,data,value) SELECT 1, \"version\", \"" . BB_VERSION . "\" WHERE NOT EXISTS(SELECT 1 FROM BBConfig WHERE id = 1)");
     $previousVersion = getInstalledVersion();
     if (previousVersion < BB_VERSION) {
         upgradeBarcodeBuddy($previousVersion);
@@ -51,7 +50,7 @@ function getInstalledVersion() {
     if ($row = $res->fetchArray()) {
         return $row["value"];
     } else {
-        die("DB Error");
+        die("DB Error: Could not get installed version");
     }
 }
 

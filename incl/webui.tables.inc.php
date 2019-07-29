86984b0318dc8154bc703b5fd03d281a50c3e28b
BuildTools <unconfigured@null.spigotmc.org>
2019-07-29 21:18:05 +0200

    List of unknown barcodes was not displayed if empty

--- incl/webui.tables.inc.php
+++ incl/webui.tables.inc.php
@@ -52,7 +52,7 @@ echo '
           </section>
        <section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
             <div class="mdl-card mdl-cell mdl-cell--12-col">
-              <div class="mdl-card__supporting-text" style="overflow-x: auto; >
+              <div class="mdl-card__supporting-text" style="overflow-x: auto;" >
                 <h4>Unknown Barcodes</h4>';
 		echo generateTableMain($barcodes, false);
 		echo '              </div>

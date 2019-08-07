8dc0bd0ad548b39d5cf62e9f8d0ef51e696d9b87
Marc Ole Bulling <Marc-Ole@gmx.de>
2019-08-07 21:03:37 +0200

    Added PHP5 websocket client, fixed crash in WS library

--- incl/websocket/client_internal.php
+++ incl/websocket/client_internal.php
@@ -1,17 +1,23 @@
 <?php
-require __DIR__ . '/php-websocket/src/Client.php';
 
+
+require_once __DIR__ . '/..//websocketconnection.inc.php';
 
 function sendWSResult($resultValue, $name) {
-global $BBCONFIG;
-$client = new \Bloatless\WebSocket\Client;
-if ($client->connect('127.0.0.1', $BBCONFIG["WS_PORT"], '/screen')) {
-
-$payload = json_encode([
-    'action' => 'echo',
-    'data' => $resultValue.$name,
-]);
-$client->sendData($payload);
-}
+    global $BBCONFIG;
+    if (isNewServerSupported()) {
+        require_once __DIR__ . '/php-websocket/src/Client.php';
+        $client = new \Bloatless\WebSocket\Client;
+    } else {
+        require_once __DIR__ . '/php-websocket-1.0/class.websocket_client.php';
+        $client = new WebsocketClient;
+    }
+        if ($client->connect('127.0.0.1', $BBCONFIG["WS_PORT"], '/screen')) {
+            $payload = json_encode(array(
+                'action' => 'echo',
+                'data' => $resultValue . $name
+            ));
+            $client->sendData($payload);
+        }
 }
 ?>
--- wsserver.php
+++ wsserver.php
@@ -27,6 +27,7 @@
 
 require_once __DIR__ . '/incl/config.php';
 require_once __DIR__ . '/incl/db.inc.php';
+require_once __DIR__ . '/incl/websocketconnection.inc.php';
 
 const ENABLE_STATISTICS = false;
 
@@ -87,20 +88,3 @@ if (isNewServerSupported()) {
     
 }
 
-function isNewServerSupported() {
-    // PHP_VERSION_ID is available as of PHP 5.2.7, if our 
-    // version is lower than that, then emulate it
-    if (!defined('PHP_VERSION_ID')) {
-        $version = explode('.', PHP_VERSION);
-        define('PHP_VERSION_ID', ($version[0] * 10000 + $version[1] * 100 + $version[2]));
-    }
-    if (PHP_VERSION_ID < 50207) {
-        define('PHP_MAJOR_VERSION', $version[0]);
-        define('PHP_MINOR_VERSION', $version[1]);
-        define('PHP_RELEASE_VERSION', $version[2]);
-    }
-    
-    return (PHP_VERSION_ID >= 70200);
-    
-    
-}

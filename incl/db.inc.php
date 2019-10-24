9fd8a466e011f6f1178d1254e4470fba09b7ae7a
Marc Ole Bulling <Marc-Ole@gmx.de>
2019-10-24 22:47:17 +0200

    Modified version number

--- incl/db.inc.php
+++ incl/db.inc.php
@@ -28,8 +28,8 @@ require_once __DIR__ . "/api.inc.php";
 require_once __DIR__ . "/websocketconnection.inc.php";
 
 
-const BB_VERSION          = "1310";
-const BB_VERSION_READABLE = "1.3.1.0";
+const BB_VERSION          = "1311";
+const BB_VERSION_READABLE = "1.3.1.1";
 
 
 

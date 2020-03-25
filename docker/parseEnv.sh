a8ddc5dc601fca0dbeef48aa10ba78270dae080a
Marc Ole Bulling <Marc-Ole@gmx.de>
2020-03-25 13:13:55 +0100

    Fixed parser

--- docker/parseEnv.sh
+++ docker/parseEnv.sh
@@ -10,19 +10,11 @@ echo "[EnvParser] Parsing environment variables"
 
 if [[ $(printenv IGNORE_SSL_CA) == "true" ]]; then
 	echo "[EnvParser] [WARNING] User requested to ignore invalid SSL certificates"
-	sed -i 's/const CURL_ALLOW_INSECURE_SSL_CA*/const CURL_ALLOW_INSECURE_SSL_CA=true"/g' /app/bbuddy/incl/config.php
-fi
-if [[ $(printenv IGNORE_SSL_CA) == "false" ]]; then
-	echo "[EnvParser] Disallowing invalid SSL certificates"
-	sed -i 's/const CURL_ALLOW_INSECURE_SSL_CA*/const CURL_ALLOW_INSECURE_SSL_CA=false"/g' /app/bbuddy/incl/config.php
+	sed -i 's/const CURL_ALLOW_INSECURE_SSL_CA.*/const CURL_ALLOW_INSECURE_SSL_CA=true;/g' /app/bbuddy/incl/config.php
 fi
 
 
 if [[ $(printenv IGNORE_SSL_HOST) == "true" ]]; then
 	echo "[EnvParser] [WARNING] User requested to ignore invalid SSL certificate hosts"
-	sed -i 's/const CURL_ALLOW_INSECURE_SSL_HOST*/const CURL_ALLOW_INSECURE_SSL_HOST=true"/g' /app/bbuddy/incl/config.php
-fi
-if [[ $(printenv IGNORE_SSL_HOST) == "false" ]]; then
-	echo "[EnvParser] Disallowing invalid SSL certificate hosts"
-	sed -i 's/const CURL_ALLOW_INSECURE_SSL_HOST*/const CURL_ALLOW_INSECURE_SSL_HOST=false"/g' /app/bbuddy/incl/config.php
+	sed -i 's/const CURL_ALLOW_INSECURE_SSL_HOST.*/const CURL_ALLOW_INSECURE_SSL_HOST=true;/g' /app/bbuddy/incl/config.php
 fi

b92078915c89662d035f734c260fba932eb0c470
hanerd <bradvah+gh@gmail.com>
2020-03-30 18:54:10 -0400

    adding feedback for screen

--- screen.php
+++ screen.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Barcode Buddy for Grocy
  *
@@ -26,8 +27,6 @@
  */
 
 
-
-
 require_once __DIR__ . "/incl/config.php";
 require_once __DIR__ . "/incl/db.inc.php";
 
@@ -35,147 +34,192 @@ require_once __DIR__ . "/incl/db.inc.php";
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Barcode Buddy Screen</title>
-    <style>
 
-      #soundbuttondiv {
-        position: fixed;
-        bottom: 10px;
-        right: 10px;
-      }
-      #title {
-        font: bold 50pt arial;
-        margin: auto;
-        padding: 10px;
-        text-align: center;
-      }
-      #mode {
-        font: bold 10pt arial;
-        margin: auto;
-        text-align: center;
-      }
-      #subtitle {
-        font: bold 20pt arial;
-        margin: auto;
-        padding: 10px;
-        text-align: center;
-      }
-      .sound {
-        background-color : #31B0D5;
-        color: white;
-        padding: 1em 2em;
-        border-radius: 4px;
-        border-color: #46b8da;
-      }
-	#muteimg {
-	  height: 2em;
-	  width: 2em;
-	}
-@media only screen and (orientation: portrait) and not (display-mode: fullscreen) {
-        .sound {
-          padding: 2em 4em;
-	}
-	#muteimg {
-	  height: 3em;
-	  width: 3em;
-	}
-}
-    </style>
-
-  </head>
-  <body bgcolor="#f6ff94">
-  <script src="./incl/nosleep.min.js"></script>
-  <script src="./incl/he.js"></script>
-    
-    <div id="title">Connecting...</div><br>
-    <div id="mode"></div><br><br><br>
-    <div id="subtitle">If you see this for more than a couple of seconds, please check if the websocket-server was started</div>
-
-    <audio id="beep_success" muted="muted" src="incl/websocket/beep.ogg"  type="audio/ogg" preload="auto"></audio>
-    <audio id="beep_nosuccess" muted="muted" src="incl/websocket/buzzer.ogg"  type="audio/ogg" preload="auto"></audio>
-    <div id="soundbuttondiv">
-<button class="sound" onclick="toggleSound()" id="soundbutton"><img id="muteimg" src="incl/img/mute.svg" alt="Toggle sound and wakelock"></button>
-</div>
-    <script>
-
-      var noSleep          = new NoSleep();
-      var wakeLockEnabled  = false;
-      var isFirstStart     = true;
-      
-     function toggleSound() {
-        if (!wakeLockEnabled) {
-          noSleep.enable();
-          wakeLockEnabled = true;
-      	  document.getElementById('beep_success').muted=false;
-      	  document.getElementById('beep_nosuccess').muted=false;
-      	  <?php if ($BBCONFIG["WS_FULLSCREEN"]) { echo " document.documentElement.requestFullscreen();"; }?>
-      	  document.getElementById("muteimg").src = "incl/img/unmute.svg";
-              } else {
-                noSleep.disable();
-      	  <?php if ($BBCONFIG["WS_FULLSCREEN"]) { echo " document.exitFullscreen();"; } ?>
-                wakeLockEnabled = false;
-      	  document.getElementById('beep_success').muted=true;
-      	  document.getElementById('beep_nosuccess').muted=true;
-      	  document.getElementById("muteimg").src = "incl/img/mute.svg";
-        }
-      }
-
-if(typeof(EventSource) !== "undefined") {
-  var source = new EventSource("incl/sse/sse_data.php");
-
-
-  source.onopen = function() {
-    if (isFirstStart) {
-      isFirstStart=false;
-      document.body.style.backgroundColor = '#b9ffad';
-      document.getElementById('title').textContent = 'Connected';
-      document.getElementById('subtitle').textContent = 'Waiting for barcode...';
-      var http = new XMLHttpRequest();
-      http.open("GET", "incl/sse/sse_data.php?getState");
-      http.send();
+<head>
+  <title>Barcode Buddy Screen</title>
+  <style>
+    #soundbuttondiv {
+      position: fixed;
+      bottom: 10px;
+      right: 10px;
     }
-  };
 
-  source.onmessage = function(event) {
-       var resultJson = JSON.parse(event.data);
-            var resultCode = resultJson.data.substring(0, 1);
-            var resultText = resultJson.data.substring(1);  
-      switch(resultCode) {
-        case '0':
-        document.body.style.backgroundColor = '#47ac3f';
-        document.getElementById('title').textContent = 'Scan success';
-        document.getElementById('subtitle').textContent = he.decode(resultText);
-        document.getElementById('beep_success').play();
-          break;
-        case '1':
-        document.body.style.backgroundColor = '#a2ff9b';
-        document.getElementById('title').textContent = 'Barcode looked up';
-        document.getElementById('subtitle').textContent = he.decode(resultText);
-        document.getElementById('beep_success').play();
-          break;
-        case '2':
-        document.body.style.backgroundColor = '#eaff8a';
-        document.getElementById('title').textContent = 'Unknown barcode';
-        document.getElementById('subtitle').textContent = resultText;
-        document.getElementById('beep_nosuccess').play();
-          break;
-        case '4':
-        document.getElementById('mode').textContent = 'Current Mode: '+resultText;
-          break;
-        case 'E':
-        document.body.style.backgroundColor = '#f9868b';
-        document.getElementById('title').textContent = 'Error';
-        document.getElementById('subtitle').textContent = resultText;
-          break;
+    .title {
+      font: bold 20pt arial;
+      margin: auto;
+      padding: 10px;
+      text-align: center;
+    }
+
+    #mode {
+      font: bold 20pt arial;
+      margin: auto;
+      text-align: center;
+    }
+
+    .subtitle {
+      font: bold 10pt arial;
+      margin: auto;
+      padding: 10px;
+      text-align: center;
+    }
+
+    .sound {
+      background-color: #31B0D5;
+      color: white;
+      padding: 1em 2em;
+      border-radius: 4px;
+      border-color: #46b8da;
+    }
+
+    #muteimg {
+      height: 2em;
+      width: 2em;
+    }
+
+    @media only screen and (orientation: portrait) and not (display-mode: fullscreen) {
+      .sound {
+        padding: 2em 4em;
       }
-  };
-} else {
-        document.body.style.backgroundColor = '#f9868b';
-        document.getElementById('title').textContent = 'Disconnected';
-        document.getElementById('subtitle').textContent = 'Sorry, your browser does not support server-sent events';
-}
-    </script> 
-    
-  </body>
+
+      #muteimg {
+        height: 3em;
+        width: 3em;
+      }
+    }
+  </style>
+
+</head>
+
+<body bgcolor="#f6ff94">
+  <script src="./incl/nosleep.min.js"></script>
+
+  <script>
+    function sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
+  </script>
+
+  <script src="./incl/he.js"></script>
+  <div align="center">
+    <div id="status">
+      <span id="mode"></span><br><br><br>
+    </div>
+
+    <div>
+      <span id="event" class="title">If you see this for more than a couple of seconds, please check if the
+        websocket-server was started</span>
+    </div>
+    <audio id="beep_success" muted="muted" src="incl/websocket/beep.ogg" type="audio/ogg" preload="auto"></audio>
+    <audio id="beep_nosuccess" muted="muted" src="incl/websocket/buzzer.ogg" type="audio/ogg" preload="auto"></audio>
+    <div id="soundbuttondiv">
+      <button class="sound" onclick="toggleSound()" id="soundbutton"><img id="muteimg" src="incl/img/mute.svg" alt="Toggle sound and wakelock"></button>
+    </div>
+    <br>
+    <div id="log">
+      <span class="title"> Previous Scans: </span>
+      <span id="log-entries" class="subtitle"></span>
+      <br>
+      <br>
+      <span id="socket-status" class="subtitle">Connecting...</span>
+
+    </div>
+
+
+  </div>
+
+  <script>
+    var noSleep = new NoSleep();
+    var wakeLockEnabled = false;
+    var isFirstStart = true;
+
+    function toggleSound() {
+      if (!wakeLockEnabled) {
+        noSleep.enable();
+        wakeLockEnabled = true;
+        document.getElementById('beep_success').muted = false;
+        document.getElementById('beep_nosuccess').muted = false;
+        <?php if ($BBCONFIG["WS_FULLSCREEN"]) {
+          echo " document.documentElement.requestFullscreen();";
+        } ?>
+        document.getElementById("muteimg").src = "incl/img/unmute.svg";
+      } else {
+        noSleep.disable();
+        <?php if ($BBCONFIG["WS_FULLSCREEN"]) {
+          echo " document.exitFullscreen();";
+        } ?>
+        wakeLockEnabled = false;
+        document.getElementById('beep_success').muted = true;
+        document.getElementById('beep_nosuccess').muted = true;
+        document.getElementById("muteimg").src = "incl/img/mute.svg";
+      }
+    }
+
+    if (typeof(EventSource) !== "undefined") {
+      var source = new EventSource("incl/sse/sse_data.php");
+
+
+      source.onopen = function() {
+        if (isFirstStart) {
+          isFirstStart = false;
+          document.body.style.backgroundColor = '#b9ffad';
+          document.getElementById('socket-status').textContent = 'Connected';
+          document.getElementById('event').textContent = 'Waiting for barcode...';
+          var http = new XMLHttpRequest();
+          http.open("GET", "incl/sse/sse_data.php?getState");
+          http.send();
+        }
+      };
+
+      async function feedbackUpdate() {
+        await sleep(2000);
+        document.getElementById('event').textContent = 'Waiting for barcode...';
+      };
+
+      source.onmessage = async function(event) {
+        var resultJson = JSON.parse(event.data);
+        var resultCode = resultJson.data.substring(0, 1);
+        var resultText = resultJson.data.substring(1);
+        switch (resultCode) {
+          case '0':
+            document.body.style.backgroundColor = '#47ac3f';
+            document.getElementById('socket-status').textContent = 'Scan success';
+            document.getElementById('event').textContent = he.decode(resultText);
+            document.getElementById('beep_success').play();
+            await feedbackUpdate();
+            document.getElementById('log-entries').innerText = '\r\n' + he.decode(resultText) + document.getElementById('log-entries').innerText;
+
+
+            break;
+          case '1':
+            document.body.style.backgroundColor = '#a2ff9b';
+            document.getElementById('title').textContent = 'Barcode looked up';
+            document.getElementById('subtitle').textContent = he.decode(resultText);
+            document.getElementById('beep_success').play();
+            break;
+          case '2':
+            document.body.style.backgroundColor = '#eaff8a';
+            document.getElementById('title').textContent = 'Unknown barcode';
+            document.getElementById('subtitle').textContent = resultText;
+            document.getElementById('beep_nosuccess').play();
+            break;
+          case '4':
+            document.getElementById('mode').innerText = 'Mode: ' + '\r\n' + resultText;
+            break;
+          case 'E':
+            document.body.style.backgroundColor = '#f9868b';
+            document.getElementById('title').textContent = 'Error';
+            document.getElementById('subtitle').textContent = resultText;
+            break;
+        }
+      };
+    } else {
+      document.body.style.backgroundColor = '#f9868b';
+      document.getElementById('title').textContent = 'Disconnected';
+      document.getElementById('subtitle').textContent = 'Sorry, your browser does not support server-sent events';
+    }
+  </script>
+
+</body>
+
 </html>

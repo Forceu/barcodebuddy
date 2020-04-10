5b232c3faee6a020d36fcbdc28e9bd8a41a67d18
Marc Ole Bulling <Marc-Ole@gmx.de>
2020-04-10 13:19:16 +0200

    Removed scrollbar introduced in commit #34, added favicon to screen as well

--- screen.php
+++ screen.php
@@ -25,94 +25,141 @@ $CONFIG->checkIfAuthenticated(true);
 <!DOCTYPE html>
 <html>
   <head>
+
+
+        <link rel="apple-touch-icon" sizes="57x57" href="./incl/img/favicon/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="./incl/img/favicon/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="./incl/img/favicon/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="./incl/img/favicon/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="./incl/img/favicon/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="./incl/img/favicon/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="./incl/img/favicon/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="./incl/img/favicon/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="./incl/img/favicon/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="./incl/img/favicon/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="./incl/img/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="./incl/img/favicon/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="./incl/img/favicon/favicon-16x16.png">
+        <meta name="msapplication-TileImage" content="./incl/img/favicon/ms-icon-144x144.png">
+        <meta name="msapplication-navbutton-color" content="#ccc">
+        <meta name="msapplication-TileColor" content="#ccc">
+        <meta name="apple-mobile-web-app-status-bar-style" content="#ccc">
+        <meta name="theme-color" content="#ccc">
+
+
     <title>Barcode Buddy Screen</title>
     <style>
-      body, html{
-          padding:0;
-          margin:0;
-          position:relative;
-          height:100%
-      }
-      .header {
-          width:100%;
-          background:#ccc;
-          padding:10px;
-          box-sizing:border-box;
-          text-transform: lowercase;
-      }
-      .content{
-          background:#eee;
-          width:100%;
-          padding:10px;
-          height:100%;
-          box-sizing:border-box;
-          padding:10px;
-          text-align: center;
-          align-content: center
-      }
-      .hdr-left {
-        text-align: center;
-        padding-left: 10px; 
-      }
-      .hdr-right {
-        float: right;
-        width: 30%;
-        text-align: right;
-        padding-right: 10px
-      }
+        body,
+        html {
+            padding: 0;
+            margin: 0;
+            position: relative;
+            height: 100%
+        }
 
-      #soundbuttondiv {
-        position: fixed;
-        bottom: 10px;
-        right: 10px;
-      }
-      .h1 {
-        font: bold 50pt arial;
-        margin: auto;
-        text-align: center;
-      }
-      .h2 {
-        font: bold 40pt arial;
-        margin: auto;
-        padding: 10px;
-        text-align: center;
-      }
-      .h3 {
-        font: bold 30pt arial;
-        margin: auto;
-        padding: 10px;
-        text-align: center;
-      }
-      .h4 {
-        font: bold 15pt arial;
-        margin: auto;
-        padding: 6px;
-      }
-      .h5 {
-        font: bold 10pt arial;
-        margin: auto;
-        text-align: center;
-      }
-      .sound {
-        background-color : #31B0D5;
-        color: white;
-        padding: 1em 2em;
-        border-radius: 4px;
-        border-color: #46b8da;
-      }
-	#muteimg {
-	  height: 2em;
-	  width: 2em;
-	}
-@media only screen and (orientation: portrait) and not (display-mode: fullscreen) {
+
+        .main-container {
+            height:100%;
+            display:flex;
+            display: -webkit-flex;
+            flex-direction: column;
+            -webkit-flex-direction: column;
+            -webkit-align-content: stretch;
+            align-content: stretch;
+        }
+
+        .header {
+            width: 100%;
+            background: #ccc;
+            padding: 10px;
+            box-sizing: border-box;
+            text-transform: lowercase;
+            flex: 0 1 auto;
+        }
+
+        .content {
+            background: #eee;
+            width: 100%;
+            padding: 10px;
+            flex: 1 0 auto;
+            box-sizing: border-box;
+            padding: 10px;
+            text-align: center;
+            align-content: center
+        }
+
+        .hdr-left {
+            text-align: center;
+            padding-left: 10px;
+        }
+
+        .hdr-right {
+            float: right;
+            width: 30%;
+            text-align: right;
+            padding-right: 10px
+        }
+
+        #soundbuttondiv {
+            position: fixed;
+            bottom: 10px;
+            right: 10px;
+        }
+
+        .h1 {
+            font: bold 4em arial;
+            margin: auto;
+            text-align: center;
+        }
+
+        .h2 {
+            font: bold 3em arial;
+            margin: auto;
+            padding: 10px;
+            text-align: center;
+        }
+
+        .h3 {
+            font: bold 2em arial;
+            margin: auto;
+            padding: 10px;
+            text-align: center;
+        }
+
+        .h4 {
+            font: bold 1.5em arial;
+            margin: auto;
+            padding: 6px;
+        }
+
+        .h5 {
+            font: bold 1em arial;
+            margin: auto;
+            text-align: center;
+        }
+
         .sound {
-          padding: 2em 4em;
-	}
-	#muteimg {
-	  height: 3em;
-	  width: 3em;
-	}
-}
+            background-color: #31B0D5;
+            color: white;
+            padding: 1em 2em;
+            border-radius: 4px;
+            border-color: #46b8da;
+        }
+
+        #muteimg {
+            height: 2em;
+            width: 2em;
+        }
+
+        @media only screen and (orientation: portrait) and not (display-mode: fullscreen) {
+            .sound {
+                padding: 2em 4em;
+            }
+            #muteimg {
+                height: 3em;
+                width: 3em;
+            }
+        }
     </style>
 
   </head>
@@ -120,6 +167,7 @@ $CONFIG->checkIfAuthenticated(true);
   <script src="./incl/js/nosleep.min.js"></script>
   <script src="./incl/js/he.js"></script>
 
+<div class="main-container">
   <div id="header" class="header">
     <span class="hdr-right h4">
       Grocy Status: <span id="grocy-sse">Connecting...</span><br>
@@ -137,7 +185,7 @@ $CONFIG->checkIfAuthenticated(true);
         </div>
       </div>
   </div>
-
+</div>
 
     <audio id="beep_success" muted="muted" src="incl/websocket/beep.ogg"  type="audio/ogg" preload="auto"></audio>
     <audio id="beep_nosuccess" muted="muted" src="incl/websocket/buzzer.ogg"  type="audio/ogg" preload="auto"></audio>
@@ -155,17 +203,17 @@ $CONFIG->checkIfAuthenticated(true);
         if (!wakeLockEnabled) {
           noSleep.enable();
           wakeLockEnabled = true;
-      	  document.getElementById('beep_success').muted=false;
-      	  document.getElementById('beep_nosuccess').muted=false;
-      	  <?php if ($BBCONFIG["WS_FULLSCREEN"]) { echo " document.documentElement.requestFullscreen();"; }?>
-      	  document.getElementById("muteimg").src = "incl/img/unmute.svg";
+          document.getElementById('beep_success').muted=false;
+          document.getElementById('beep_nosuccess').muted=false;
+          <?php if ($BBCONFIG["WS_FULLSCREEN"]) { echo " document.documentElement.requestFullscreen();"; }?>
+          document.getElementById("muteimg").src = "incl/img/unmute.svg";
               } else {
                 noSleep.disable();
-      	  <?php if ($BBCONFIG["WS_FULLSCREEN"]) { echo " document.exitFullscreen();"; } ?>
+          <?php if ($BBCONFIG["WS_FULLSCREEN"]) { echo " document.exitFullscreen();"; } ?>
                 wakeLockEnabled = false;
-      	  document.getElementById('beep_success').muted=true;
-      	  document.getElementById('beep_nosuccess').muted=true;
-      	  document.getElementById("muteimg").src = "incl/img/mute.svg";
+          document.getElementById('beep_success').muted=true;
+          document.getElementById('beep_nosuccess').muted=true;
+          document.getElementById("muteimg").src = "incl/img/mute.svg";
         }
       }
 

Forceu/barcodebuddy

commit 1a5bba1256e4543f2fbc95af6c6ef08ec6473e83

Changing global variables to Singletons (#71)
* Re-detect existing issues

* Declare missing global variable

* Declare missing global variable

* Correct return type

* Tell Psalm about db global variable

* Remove useless check, $db will always be a DatabaseConnection instance.

* Remove other ignored $db instances

* Move global declarations to the start of methods

* Gather all includes at the start of the file

* Initial attempt to remove $db global

* Initial attempt to remove $BBCONFIG global

* Initial attempt to remove $BBCONFIG global

* Merge Upstream

* Fix include path

* Refactoring, removed trailing linebreak

* Implement Iterator and Countable for BBConfig

Otherwise looping through items would not be possible

Co-authored-by: Marc Ole Bulling

incl/api.inc.php
@@ -18,6 +18,8 @@
 
 
 require_once __DIR__ . "/configProcessing.inc.php";
+require_once __DIR__ . "/db.inc.php";
+require_once __DIR__ . "/config.inc.php";
 
 const API_O_PRODUCTS     = 'objects/products';
 const API_PRODUCTS       = 'stock/products';
@@ -55,16 +57,16 @@ class CurlGenerator {
     
     function __construct($url, $method = METHOD_GET, $jasonData = null, $loginOverride = null, $noApiCall = false) {
         global $CONFIG;
+
+        $config = BBConfig::getInstance();
         
         $this->method  = $method;
         $this->urlApi  = $url;
         $this->ch      = curl_init();
 
         if ($loginOverride == null) {
-            require_once __DIR__ . "/db.inc.php";
-            global $BBCONFIG;
-            $apiKey = $BBCONFIG["GROCY_API_KEY"];
-            $apiUrl = $BBCONFIG["GROCY_API_URL"];
+            $apiKey = $config["GROCY_API_KEY"];
+            $apiUrl = $config["GROCY_API_URL"];
         } else {
             $apiKey = $loginOverride[LOGIN_API_KEY];
             $apiUrl = $loginOverride[LOGIN_URL];
@@ -99,9 +101,8 @@ class CurlGenerator {
     
     function execute($decode = false) {
         if (DISPLAY_DEBUG) {
-            global $db;
             $startTime = microtime(true);
-            $db->saveLog("<i>Executing API call: " . $this->urlApi. "</i>", false, false, true);
+            DatabaseConnection::getInstance()->saveLog("<i>Executing API call: " . $this->urlApi. "</i>", false, false, true);
         }
         $curlResult   = curl_exec($this->ch);
         $this->checkForErrorsAndThrow($curlResult);
@@ -122,7 +123,7 @@ class CurlGenerator {
         }
         if (DISPLAY_DEBUG) {
             $totalTimeMs = round((microtime(true)- $startTime) * 1000);
-            $db->saveLog("<i>Executing took " . $totalTimeMs . "ms</i>", false, false, true);
+            DatabaseConnection::getInstance()->saveLog("<i>Executing took " . $totalTimeMs . "ms</i>", false, false, true);
         }
         if ($decode)
             return $jsonDecoded;
@@ -221,9 +222,10 @@ class API {
     /**
      *   Check if API details are correct
      * 
-     * @param  String URL to Grocy API
-     * @param  String API key
-     * @return Returns String with error or true if connection could be established
+     * @param String $givenurl URL to Grocy API
+     * @param String $apikey   API key
+     *
+     * @return String | true Returns String with error or true if connection could be established
      */
     public static function checkApiConnection($givenurl, $apikey) {
         $loginInfo = array(LOGIN_URL => $givenurl, LOGIN_API_KEY => $apikey);
@@ -313,9 +315,6 @@ class API {
      * @return false if default best before date not set
      */
     public static function purchaseProduct($id, $amount, $bestbefore = null, $price = null, &$fileLock = null, $defaultBestBefore = null) {
-        require_once __DIR__ . "/db.inc.php";
-        global $BBCONFIG;
-        
         $daysBestBefore = 0;
         $data = array(
             'amount' => $amount,
@@ -346,7 +345,7 @@ class API {
         }
         if ($fileLock != null)
             $fileLock->removeLock();
-        if ($BBCONFIG["SHOPPINGLIST_REMOVE"]) {
+        if (BBConfig::getInstance()["SHOPPINGLIST_REMOVE"]) {
             self::removeFromShoppinglist($id, $amount);
         }
         return ($daysBestBefore != 0);
@@ -487,8 +486,6 @@ class API {
      * @return [String]          Returns product name or "N/A" if not found
      */
     public static function lookupNameByBarcodeInOpenFoodFacts($barcode) {
-        global $BBCONFIG;
-        
         $url = "https://world.openfoodfacts.org/api/v0/product/" . $barcode . ".json";
 
         $curl = new CurlGenerator($url, METHOD_GET, null, null, true);
@@ -520,7 +517,7 @@ class API {
             $productName = sanitizeString($result["product"]["product_name"]);
         }
 
-        if ($BBCONFIG["USE_GENERIC_NAME"]) {
+        if (BBConfig::getInstance()["USE_GENERIC_NAME"]) {
             if ($genericName != null)
                 return $genericName;
             if ($productName != null)
@@ -660,12 +657,10 @@ class API {
     }
 
     public static function logError($errorMessage, $isFatal = true) {
-        require_once __DIR__ . "/db.inc.php";
-        global $db;
-        if ($db != null)
-            $db->saveError($errorMessage, $isFatal);
+        try {
+            DatabaseConnection::getInstance()->saveError($errorMessage, $isFatal);
+        } catch (DbConnectionDuringEstablishException $_) {
+            // Error occured during the DB connection. As such, DB is not available to log the error.
+        }
     }
-    
 }
-
-?>

incl/processing.inc.php
@@ -17,50 +17,51 @@
  */
 
 require_once __DIR__ . "/lockGenerator.inc.php";
+require_once __DIR__ . "/db.inc.php";
+require_once __DIR__ . "/config.inc.php";
 
 
 // Function that is called when a barcode is passed on
 function processNewBarcode($barcodeInput, $websocketEnabled = true, $bestBeforeInDays = null, $price = null) {
-    require_once __DIR__ . "/db.inc.php";
-    global $db;
-    global $BBCONFIG;
+    $db = DatabaseConnection::getInstance();
+    $config = BBConfig::getInstance();
     
     $barcode = strtoupper($barcodeInput);
-    if ($barcode == $BBCONFIG["BARCODE_C"]) {
+    if ($barcode == $config["BARCODE_C"]) {
         $db->setTransactionState(STATE_CONSUME);
         return outputLog("Set state to Consume", EVENT_TYPE_MODE_CHANGE, true, $websocketEnabled);
     }
-    if ($barcode == $BBCONFIG["BARCODE_CS"]) {
+    if ($barcode == $config["BARCODE_CS"]) {
         $db->setTransactionState(STATE_CONSUME_SPOILED);
         return outputLog("Set state to Consume (spoiled)", EVENT_TYPE_MODE_CHANGE, true, $websocketEnabled);
     }
-    if ($barcode == $BBCONFIG["BARCODE_CA"]) {
+    if ($barcode == $config["BARCODE_CA"]) {
         $db->setTransactionState(STATE_CONSUME_ALL);
         return outputLog("Set state to Consume all", EVENT_TYPE_MODE_CHANGE, true, $websocketEnabled);
     }
-    if ($barcode == $BBCONFIG["BARCODE_P"]) {
+    if ($barcode == $config["BARCODE_P"]) {
         $db->setTransactionState(STATE_PURCHASE);
         return outputLog("Set state to Purchase", EVENT_TYPE_MODE_CHANGE, true, $websocketEnabled);
     }
-    if ($barcode == $BBCONFIG["BARCODE_O"]) {
+    if ($barcode == $config["BARCODE_O"]) {
         $db->setTransactionState(STATE_OPEN);
         return outputLog("Set state to Open", EVENT_TYPE_MODE_CHANGE, true, $websocketEnabled);
     }
-    if ($barcode == $BBCONFIG["BARCODE_GS"]) {
+    if ($barcode == $config["BARCODE_GS"]) {
         $db->setTransactionState(STATE_GETSTOCK);
         return outputLog("Set state to Inventory", EVENT_TYPE_MODE_CHANGE, true, $websocketEnabled);
     }
-    if ($barcode == $BBCONFIG["BARCODE_AS"]) {
+    if ($barcode == $config["BARCODE_AS"]) {
         $db->setTransactionState(STATE_ADD_SL);
         return outputLog("Set state to Shopping list", EVENT_TYPE_MODE_CHANGE, true, $websocketEnabled);
     }
-    if (stringStartsWith($barcode, $BBCONFIG["BARCODE_Q"])) {
-        $quantitiy = str_replace($BBCONFIG["BARCODE_Q"], "", $barcode);
+    if (stringStartsWith($barcode, $config["BARCODE_Q"])) {
+        $quantitiy = str_replace($config["BARCODE_Q"], "", $barcode);
         checkIfNumeric($quantitiy);
-        if ($BBCONFIG["LAST_PRODUCT"] != null) {
-            $lastBarcode = $BBCONFIG["LAST_BARCODE"] . " (" . $BBCONFIG["LAST_PRODUCT"] . ")";
+        if ($config["LAST_PRODUCT"] != null) {
+            $lastBarcode = $config["LAST_BARCODE"] . " (" . $config["LAST_PRODUCT"] . ")";
         } else {
-            $lastBarcode = $BBCONFIG["LAST_BARCODE"];
+            $lastBarcode = $config["LAST_BARCODE"];
         }
         changeQuantityAfterScan($quantitiy);
         return outputLog("Set quantitiy to $quantitiy for barcode $lastBarcode", EVENT_TYPE_MODE_CHANGE, true, $websocketEnabled);
@@ -120,10 +121,9 @@ const WS_RESULT_ERROR                 = 'E';
 
 //Save a log input to the database or submit websocket
 function outputLog($log, $eventType, $isVerbose = false, $websocketEnabled = true, $websocketResultCode = WS_RESULT_PRODUCT_FOUND, $websocketText = null) {
-    require_once __DIR__ . "/db.inc.php";
     global $LOADED_PLUGINS;
-    global $db;
-    $db->saveLog($log, $isVerbose);
+
+    DatabaseConnection::getInstance()->saveLog($log, $isVerbose);
     if ($websocketText == null) {
         $websocketText = $log;
     }
@@ -136,9 +136,7 @@ function outputLog($log, $eventType, $isVerbose = false, $websocketEnabled = tru
 
 //Execute a chore when chore barcode was submitted
 function processChoreBarcode($barcode) {
-    require_once __DIR__ . "/db.inc.php";
-    global $db;
-    $id = $db->getChoreBarcode(sanitizeString($barcode))['choreId'];
+    $id = DatabaseConnection::getInstance()->getChoreBarcode(sanitizeString($barcode))['choreId'];
     checkIfNumeric($id);
     API::executeChore( $id);
     return sanitizeString(API::getChoresInfo($id)["name"]);
@@ -146,8 +144,7 @@ function processChoreBarcode($barcode) {
 
 //If grocy does not know this barcode
 function processUnknownBarcode($barcode, $websocketEnabled, &$fileLock, $bestBeforeInDays, $price) {
-    require_once __DIR__ . "/db.inc.php";
-    global $db;
+    $db = DatabaseConnection::getInstance();
     $amount = 1;
     if ($db->getTransactionState() == STATE_PURCHASE) {
         $amount = $db->getQuantityByBarcode($barcode);
@@ -218,8 +215,7 @@ function changeWeightTareItem($barcode, $newWeight) {
 
 //Change mode if was supplied by GET parameter
 function processModeChangeGetParameter($modeParameter) {
-    require_once __DIR__ . "/db.inc.php";
-    global $db;
+    $db = DatabaseConnection::getInstance();
     switch (trim($modeParameter)) {
         case "consume":
         $db->setTransactionState(STATE_CONSUME);
@@ -245,11 +241,9 @@ function processModeChangeGetParameter($modeParameter) {
 
 //This will be called when a new grocy product is created from BB and the grocy tab is closed
 function processRefreshedBarcode($barcode) {
-    require_once __DIR__ . "/db.inc.php";
-    global $db;
     $productInfo = API::getProductByBardcode($barcode);
     if ($productInfo != null) {
-        $db->updateSavedBarcodeMatch($barcode, $productInfo["id"]);
+        DatabaseConnection::getInstance()->updateSavedBarcodeMatch($barcode, $productInfo["id"]);
     }
 }
 
@@ -258,9 +252,8 @@ const USE_NEW_INVENTORY_API = false;
 
     // Process a barcode that Grocy already knows
 function processKnownBarcode($productInfo, $barcode, $websocketEnabled, &$fileLock, $bestBeforeInDays, $price) {
-    require_once __DIR__ . "/db.inc.php";
-    global $BBCONFIG;
-    global $db;
+    $config = BBConfig::getInstance();
+    $db = DatabaseConnection::getInstance();
 
     $output = "Undefined";
 
@@ -297,7 +290,7 @@ function processKnownBarcode($productInfo, $barcode, $websocketEnabled, &$fileLo
             } else {
                 $output = outputLog("Product found. None in stock, not consuming: " . $productInfo["name"] . ". Barcode: " . $barcode, EVENT_TYPE_ADD_KNOWN_BARCODE, false, $websocketEnabled, WS_RESULT_PRODUCT_FOUND, "Product found. None in stock, not consuming: " . $productInfo["name"]);
             }
-            if ($BBCONFIG["REVERT_SINGLE"]) {
+            if ($config["REVERT_SINGLE"]) {
                 $db->saveLog("Reverting back to Consume", true);
                 $db->setTransactionState(STATE_CONSUME);
             }
@@ -307,7 +300,7 @@ function processKnownBarcode($productInfo, $barcode, $websocketEnabled, &$fileLo
             $output = outputLog("Product found. Consuming 1 spoiled " . $productInfo["unit"] . " of " . $productInfo["name"] . ". Barcode: " . $barcode, EVENT_TYPE_ADD_KNOWN_BARCODE, false, $websocketEnabled, WS_RESULT_PRODUCT_FOUND, "Consuming 1 spoiled " . $productInfo["unit"] . " of " . $productInfo["name"]);
             API::consumeProduct($productInfo["id"], 1, true);
             $fileLock->removeLock();
-            if ($BBCONFIG["REVERT_SINGLE"]) {
+            if ($config["REVERT_SINGLE"]) {
                 $db->saveLog("Reverting back to Consume", true);
                 $db->setTransactionState(STATE_CONSUME);
             }
@@ -319,7 +312,6 @@ function processKnownBarcode($productInfo, $barcode, $websocketEnabled, &$fileLo
             else
                 $additionalLog = "";
             $output = outputLog("Product found. Adding  $amount " . $productInfo["unit"] . " of " . $productInfo["name"] . ". Barcode: " . $barcode . $additionalLog, EVENT_TYPE_ADD_KNOWN_BARCODE, false, $websocketEnabled, WS_RESULT_PRODUCT_FOUND, "Adding $amount " . $productInfo["unit"] . " of " . $productInfo["name"] . $additionalLog);
-            $isBestBeforeSet = API::purchaseProduct($productInfo["id"], $amount, $bestBeforeInDays, $price, $fileLock, $productInfo["defaultBestBefore"]);
             //no $fileLock->removeLock() needed, as it is done in API::purchaseProduct
             return $output;
         case STATE_OPEN:
@@ -330,7 +322,7 @@ function processKnownBarcode($productInfo, $barcode, $websocketEnabled, &$fileLo
                 $output = outputLog("Product found. None in stock, not opening: " . $productInfo["name"] . ". Barcode: " . $barcode, EVENT_TYPE_ADD_KNOWN_BARCODE, false, $websocketEnabled, WS_RESULT_PRODUCT_FOUND, "Product found. None in stock, not opening: " . $productInfo["name"]);
             }
             $fileLock->removeLock();
-            if ($BBCONFIG["REVERT_SINGLE"]) {
+            if ($config["REVERT_SINGLE"]) {
                 $db->saveLog("Reverting back to Consume", true);
                 $db->setTransactionState(STATE_CONSUME);
             }
@@ -357,14 +349,13 @@ function processKnownBarcode($productInfo, $barcode, $websocketEnabled, &$fileLo
 }
 
 function getQuantitiyForBarcode($barcode, $isConsume, $productInfo) {
-    global $BBCONFIG;
-    global $db;
+    $config = BBConfig::getInstance();
 
-    if ($isConsume && !$BBCONFIG["CONSUME_SAVED_QUANTITY"])
+    if ($isConsume && !$config["CONSUME_SAVED_QUANTITY"])
         return 1;
-    if ($BBCONFIG["USE_GROCY_QU_FACTOR"])
+    if ($config["USE_GROCY_QU_FACTOR"])
         return intval($productInfo["quFactor"]);
-    return $amount = $db->getQuantityByBarcode($barcode);
+    return $amount = DatabaseConnection::getInstance()->getQuantityByBarcode($barcode);
 }
 
 
@@ -406,8 +397,6 @@ function checkIfNumeric($input) {
 
 //Generate checkboxes for web ui
 function explodeWordsAndMakeCheckboxes($words, $id) {
-    require_once __DIR__ . "/db.inc.php";
-    global $db;
     if ($words == "N/A") {
         return "";
     }
@@ -416,7 +405,7 @@ function explodeWordsAndMakeCheckboxes($words, $id) {
     $i           = 0;
     foreach ($cleanWords as $str) {
     $tagWord = trim($str);
-        if (strlen($tagWord) > 0 && $db->tagNotUsedYet($tagWord)) {
+        if (strlen($tagWord) > 0 && DatabaseConnection::getInstance()->tagNotUsedYet($tagWord)) {
             $selections = $selections . '<label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="checkbox-' . $id . '_' . $i . '">
   <input type="checkbox"  value="' . $tagWord . '" name="tags[' . $id . '][' . $i . ']" id="checkbox-' . $id . '_' . $i . '" class="mdl-checkbox__input">
   <span class="mdl-checkbox__label">' . $tagWord . '</span>
@@ -435,12 +424,12 @@ function cleanNameForTagLookup($input) {
 
 //If a quantity barcode was scanned, add the quantitiy and process further
 function changeQuantityAfterScan($amount) {
-    require_once __DIR__ . "/db.inc.php";
-    global $BBCONFIG;
-    global $db;
-    $barcode = sanitizeString($BBCONFIG["LAST_BARCODE"]);
-    if ($BBCONFIG["LAST_PRODUCT"] != null) {
-        $db->addUpdateQuantitiy($barcode, $amount, $BBCONFIG["LAST_PRODUCT"]);
+    $config = BBConfig::getInstance();
+
+    $db = DatabaseConnection::getInstance();
+    $barcode = sanitizeString($config["LAST_BARCODE"]);
+    if ($config["LAST_PRODUCT"] != null) {
+        $db->addUpdateQuantitiy($barcode, $amount, $config["LAST_PRODUCT"]);
     } else {
         $product = API::getProductByBardcode($barcode);
         if ($product != null) {
@@ -457,9 +446,7 @@ function changeQuantityAfterScan($amount) {
 
 //Merge tags and product info
 function getAllTags() {
-    require_once __DIR__ . "/db.inc.php";
-    global $db;
-    $tags       = $db->getStoredTags();
+    $tags       = DatabaseConnection::getInstance()->getStoredTags();
     $products   = API::getProductInfo();
     $returnTags = array();
     
@@ -490,10 +477,8 @@ function sortChores($a,$b) {
 
 //Merges chores with chore info
 function getAllChores() {
-    require_once __DIR__ . "/db.inc.php";
-    global $db;
     $chores = API::getChoresInfo();
-    $barcodes = $db->getStoredChoreBarcodes();
+    $barcodes = DatabaseConnection::getInstance()->getStoredChoreBarcodes();
     $returnChores = array();
 
     foreach ($chores as $chore) {

.github/workflows/baseline.xml
@@ -11,10 +11,6 @@
     </UnresolvableInclude>
   </file>
   <file src="incl/api.inc.php">
-    <InvalidGlobal occurrences="2">
-      <code>global $BBCONFIG;</code>
-      <code>global $db;</code>
-    </InvalidGlobal>
     <InvalidReturnStatement occurrences="8">
       <code>"Could not connect to server&lt;br&gt;"</code>
       <code>"Error: ". $e-&gt;getMessage()</code>
@@ -50,14 +46,8 @@
       <code>none</code>
       <code>none</code>
     </UndefinedDocblockClass>
-    <UndefinedVariable occurrences="1">
-      <code>$db</code>
-    </UndefinedVariable>
   </file>
   <file src="incl/authentication/authentication.inc.php">
-    <InvalidGlobal occurrences="1">
-      <code>global $CONFIG;</code>
-    </InvalidGlobal>
     <UndefinedClass occurrences="3">
       <code>\Delight\Db\PdoDsn</code>
       <code>\Delight\Auth\Auth</code>
@@ -68,9 +58,6 @@
     <ForbiddenCode occurrences="1">
       <code>var_dump($variable)</code>
     </ForbiddenCode>
-    <InvalidGlobal occurrences="1">
-      <code>global $CONFIG;</code>
-    </InvalidGlobal>
     <InvalidOperand occurrences="1">
       <code>$subnetMask</code>
     </InvalidOperand>
@@ -166,16 +153,6 @@
     <InvalidOperand occurrences="1">
       <code>$result</code>
     </InvalidOperand>
-  </file>
-  <file src="screen.php">
-    <UndefinedGlobalVariable occurrences="1">
-      <code>$BBCONFIG</code>
-    </UndefinedGlobalVariable>
-  </file>
-  <file src="setup.php">
-    <UndefinedGlobalVariable occurrences="1">
-      <code>$BBCONFIG</code>
-    </UndefinedGlobalVariable>
   </file>
   <file src="wsserver.php">
     <MissingFile occurrences="1">

.github/workflows/psalm.xml
@@ -17,4 +17,7 @@
             <directory name="./vendor"/>
         </ignoreFiles>
     </projectFiles>
+    <globals>
+        <var name="BBCONFIG" type="array" />
+    </globals>
 </psalm>

api/index.php
@@ -21,6 +21,7 @@
 require_once __DIR__ . "/../incl/configProcessing.inc.php";
 require_once __DIR__ . "/../incl/db.inc.php";
 require_once __DIR__ . "/../incl/processing.inc.php";
+require_once __dir__ . "/../incl/config.inc.php";
 
 //removes Get paramterss
 $requestedUrl = strtok($_SERVER["REQUEST_URI"], '?');
@@ -44,7 +45,6 @@ class BBuddyApi {
     private $routes = array();
     
     function checkIfAuthorized() {
-        global $db;
         global $CONFIG;
         
         if ($CONFIG->checkIfAuthenticated(false))
@@ -59,7 +59,7 @@ class BBuddyApi {
         if ($apiKey == "")
             self::sendUnauthorizedAndDie();
         
-        if ($db->isValidApiKey($apiKey))
+        if (DatabaseConnection::getInstance()->isValidApiKey($apiKey))
             return true;
         else
             self::sendUnauthorizedAndDie();
@@ -137,34 +137,32 @@ class BBuddyApi {
         }));
         
         $this->addRoute(new ApiRoute("/state/getmode", function() {
-            global $db;
             return self::createResultArray(array(
-                "mode" => $db->getTransactionState()
+                "mode" => DatabaseConnection::getInstance()->getTransactionState()
             ));
         }));
         
         $this->addRoute(new ApiRoute("/state/setmode", function() {
-            global $db;
             //Also check if value is a valid range (STATE_CONSUME the lowest and STATE_CONSUME_ALL the highest value)
             if (!isset($_POST["state"]) || !is_numeric($_POST["state"]) || $_POST["state"] < STATE_CONSUME || $_POST["state"] > STATE_CONSUME_ALL)
                 return self::createResultArray(null, "Invalid state provided", 400);
             else {
-                $db->setTransactionState(intval($_POST["state"]));
+                DatabaseConnection::getInstance()->setTransactionState(intval($_POST["state"]));
                 return self::createResultArray();
             }
         }));
         
         $this->addRoute(new ApiRoute("/system/barcodes", function() {
-            global $BBCONFIG;
+            $config = BBConfig::getInstance();
             return self::createResultArray(array(
-                "BARCODE_C" => $BBCONFIG["BARCODE_C"],
-                "BARCODE_CS" => $BBCONFIG["BARCODE_CS"],
-                "BARCODE_P" => $BBCONFIG["BARCODE_P"],
-                "BARCODE_O" => $BBCONFIG["BARCODE_O"],
-                "BARCODE_GS" => $BBCONFIG["BARCODE_GS"],
-                "BARCODE_Q" => $BBCONFIG["BARCODE_Q"],
-                "BARCODE_AS" => $BBCONFIG["BARCODE_AS"],
-                "BARCODE_CA" => $BBCONFIG["BARCODE_CA"]
+                "BARCODE_C" => $config["BARCODE_C"],
+                "BARCODE_CS" => $config["BARCODE_CS"],
+                "BARCODE_P" => $config["BARCODE_P"],
+                "BARCODE_O" => $config["BARCODE_O"],
+                "BARCODE_GS" => $config["BARCODE_GS"],
+                "BARCODE_Q" => $config["BARCODE_Q"],
+                "BARCODE_AS" => $config["BARCODE_AS"],
+                "BARCODE_CA" => $config["BARCODE_CA"]
             ));
         }));
         

incl/config.inc.php
@@ -1,0 +1,143 @@
+<?php
+
+include_once __DIR__ . "/processing.inc.php";
+
+class BBConfig implements ArrayAccess, Iterator, Countable {
+    /**
+     * @var null|BBConfig
+     */
+    private static $_BBConfigInstance = null;
+    
+    private $container = array();
+    
+    /**
+     * BBConfig constructor.
+     *
+     * @param $db DatabaseConnection
+     */
+    private function __construct($db) {
+        global $CONFIG;
+        
+        $res = $db->getRawConfig();
+        while ($row = $res->fetchArray()) {
+            if (isset($CONFIG->OVERRIDDEN_USER_CONFIG[$row['data']]))
+                $this->container[$row['data']] = $CONFIG->OVERRIDDEN_USER_CONFIG[$row['data']];
+            else
+                $this->container[$row['data']] = $row['value'];
+        }
+        if (sizeof($this->container) == 0) {
+            die("DB Error: Could not get configuration");
+        }
+        
+        if ($CONFIG->EXTERNAL_GROCY_URL != null)
+            $this->container["GROCY_BASE_URL"] = $CONFIG->EXTERNAL_GROCY_URL;
+        else
+            $this->container["GROCY_BASE_URL"] = strrtrim($this->container["GROCY_API_URL"], "api/");
+        
+        if (substr($this->container["GROCY_BASE_URL"], -1) != "/") {
+            if (substr($this->container["GROCY_BASE_URL"], -1) != "/") {
+                $this->container["GROCY_BASE_URL"] .= "/";
+                $this->container["GROCY_BASE_URL"] .= "/";
+            }
+        }
+    }
+    
+    /**
+     * Get an instance of DatabaseConnection
+     * If an existing instance is available, it will be used.
+     * If not available, and no instance is being created, a new connection will be established.
+     * Otherwise (such as during an ongoing upgrade in this php instance) an error will be thrown
+     *
+     * @param null|DatabaseConnection $db
+     *
+     * @return BBConfig
+     */
+    static function getInstance($db = null) {
+        if (self::$_BBConfigInstance != null) {
+            return self::$_BBConfigInstance;
+        }
+        
+        self::$_BBConfigInstance = new BBConfig($db ? $db : DatabaseConnection::getInstance());
+        return self::$_BBConfigInstance;
+    }
+    
+    /**
+     * Force config to update from the DB
+     */
+    public static function forceRefresh() {
+        self::$_BBConfigInstance = null;
+    }
+    
+    /**
+     * Set a key to the given value in the running copy of the DB-stored config
+     *
+     * @param null|string $offset Configuration Key
+     *
+     * @param string $value Configuration Value
+     *
+     * @deprecated N.B. If called directly, this config change will NOT be persisted in the database.
+     */
+    public function offsetSet($offset, $value) {
+        if (is_null($offset)) {
+            $this->container[] = $value;
+        } else {
+            $this->container[$offset] = $value;
+        }
+    }
+    
+    /**
+     * Check if a given key exists in the config
+     *
+     * @param string $offset
+     * @return bool
+     */
+    public function offsetExists($offset) {
+        return isset($this->container[$offset]);
+    }
+    
+    /**
+     * Delete a given key from the DB-stored config
+     *
+     * @param string $offset
+     *
+     * * @deprecated N.B. If called directly, this config change will NOT be persisted in the database.
+     */
+    public function offsetUnset($offset) {
+        unset($this->container[$offset]);
+    }
+    
+    /**
+     * Get the value for a given key from the DB-stored config
+     * @param string $offset
+     *
+     * @return string|null
+     */
+    public function offsetGet($offset) {
+        return isset($this->container[$offset]) ? $this->container[$offset] : null;
+    }
+    
+    public function rewind() {
+        reset($this->container);
+    }
+    
+    public function current() {
+        return current($this->container);
+    }
+    
+    public function key() {
+        return key($this->container);
+    }
+    
+    public function next() {
+        return next($this->container);
+    }
+    
+    public function valid() {
+        return $this->current() !== false;
+    }
+    
+    public function count() {
+        return count($this->container);
+    }
+    
+}

commit c3d925210416303c4365305655dc1a8aaa486748

Fixed problem where purchased products were not added to stock, introduced in 1a5bba1256e4543f2fbc95af6c6ef08ec6473e83

incl/api.inc.php
@@ -337,20 +337,21 @@ class API {
         self::logError("Grocy did not provide version number");
         return null;
     }
-    
-    
+
+
     /**
      *
      *  Adds a Grocy product.
-     * 
-     * @param  String id of product
-     * @param  int amount of product
-     * @param  String Date of best before Default: null (requests default BestBefore date from grocy)
-     * @param  String price of product Default: null
+     *
+     * @param $id
+     * @param $amount
+     * @param null $bestbefore
+     * @param null $price
+     * @param null $fileLock
+     * @param null $defaultBestBefore
      * @return false if default best before date not set
      */
     public static function purchaseProduct($id, $amount, $bestbefore = null, $price = null, &$fileLock = null, $defaultBestBefore = null) {
-        $daysBestBefore = 0;
         $data = array(
             'amount' => $amount,
             'transaction_type' => 'purchase'

incl/processing.inc.php
@@ -388,6 +388,7 @@ function processKnownBarcode($productInfo, $barcode, $websocketEnabled, &$fileLo
                 ->setWebsocketResultCode(WS_RESULT_PRODUCT_FOUND)
                 ->addProductFoundText()
                 ->createLog();
+            API::purchaseProduct($productInfo["id"], $amount, $bestBeforeInDays, $price, $fileLock, $productInfo["defaultBestBefore"]);
             //no $fileLock->removeLock() needed, as it is done in API::purchaseProduct
             return $output;
         case STATE_OPEN:
91198fcf50de7f1d8b06b05a0f3d9937064c5a1b
Marc Ole Bulling <Marc-Ole@gmx.de>
2020-07-06 19:59:24 +0200

    Update version number

--- incl/configProcessing.inc.php
+++ incl/configProcessing.inc.php
@@ -17,8 +17,8 @@
  */
 
 
-const BB_VERSION = "1504";
-const BB_VERSION_READABLE = "1.5.0.5-dev";
+const BB_VERSION = "1510";
+const BB_VERSION_READABLE = "1.5.1.0";
 
 const CONFIG_PATH = __DIR__ . '/../data/config.php';
 const AUTHDB_PATH = __DIR__ . '/../data/users.db';

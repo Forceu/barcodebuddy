9c3cd1221dff85e601be6e7ccf86ffe2d319b17b
Ole-Kenneth <ok@rait.no>
2021-02-26 06:32:45 +0100

    Add guard for missing HTTP_ACCEPT_LANGUAGE

--- incl/locale.inc.php
+++ incl/locale.inc.php
@@ -22,7 +22,7 @@ foreach(array_diff(scandir(__DIR__ . "/../locale", 1), ['..', '.']) as $lang) {
     $available_languages[substr(strtolower($lang), 0, 2)] = $lang;
 }
 
-$lang = strtolower(substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2));
+$lang = strtolower(substr($_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? 'en-US', 0, 2));
 $locale = $available_languages[$lang] ?? 'en';
 
 putenv("LC_ALL=$locale");

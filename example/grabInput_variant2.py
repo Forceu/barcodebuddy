44495844eaaaae6b740a360b752aa9aa8095e38e
Philipp Christoph <contact@philippchristoph.de>
2021-04-07 10:54:33 +0200

    Add retry to grabInput_variant2. Add ability to perform generic actions on connect/scan/exception.

--- example/grabInput_variant2.py
+++ example/grabInput_variant2.py
@@ -1,28 +1,46 @@
 #!/usr/bin/python
 
 from evdev import InputDevice, categorize, ecodes
+from retrying import retry
 import requests
 import yaml
+import sys
 
+class ScanEventHandler:
+  def onConnect(self):
+    # Perform an action on (re-)connect of the scanner.
+    pass
+
+  def onScan(self, barcode):
+    # Perform an action once a barcode was scanned (e.g. send via API)
+    requests.get(f'{SERVER_ADDRESS}action/scan?apikey={API_KEY}&add={barcode}')
+
+  def onException(self, exception):
+    # Perform an action on any exception (can also be disconnect).
+    pass
 
 with open('config.yaml') as f:
     config = yaml.safe_load(f)
 
 SERVER_ADDRESS = config['server_address']
 API_KEY = config['api_key']
-device_id = config['device_id']
-device = InputDevice(f'/dev/input/event{device_id}')
+DEVICE_ID = config['device_id']
 
-scancodes = {
-    1:  u'?1',
-    2:  u'1',
-    3:  u'2',
-    4:  u'3',
-    5:  u'4',
-    6:  u'5',
-    7:  u'6',
-    8:  u'7',
-    9:  u'8',
+HEADERS = {
+    'Authorization': f'Bearer {HA_TOKEN}',
+    'content-type': 'application/json',
+}
+
+SCAN_CODES = {
+    1: u'?1',
+    2: u'1',
+    3: u'2',
+    4: u'3',
+    5: u'4',
+    6: u'5',
+    7: u'6',
+    8: u'7',
+    9: u'8',
     10: u'9',
     11: u'0',
     12: u'-',
@@ -67,21 +85,36 @@ scancodes = {
 }
 NOT_RECOGNIZED_KEY = u'?'
 
-barcode = ''
+scanEventHandler = ScanEventHandler()
 
-print('Please begin scanning...')
+@retry(wait_fixed=2000)
+def scan_barcodes_loop():
+    try:
+        device = InputDevice(f'/dev/input/event{DEVICE_ID}')
+        barcode = ''
 
-for event in device.read_loop():
-    if event.type == ecodes.EV_KEY:
-        eventdata = categorize(event)
-        if eventdata.keystate == 1:
-            scancode = eventdata.scancode
-            if scancode == 28:
-                print(f'Sending: {barcode}')
-                requests.get(f'{SERVER_ADDRESS}action/scan?apikey={API_KEY}&add={barcode}')
-                barcode = ''
-            else:
-                key = scancodes.get(scancode, NOT_RECOGNIZED_KEY)
-                barcode = barcode + key
-            if key == NOT_RECOGNIZED_KEY:
-                print(f'unknown key, scancode={scancode}')
+        print('Please begin scanning...')
+        scanEventHandler.onConnect()
+
+        for event in device.read_loop():
+            if event.type == ecodes.EV_KEY:
+                event_data = categorize(event)
+                if event_data.keystate == 1:
+                    scancode = event_data.scancode
+                    if scancode == 28:
+                        print(f'Sending: {barcode}')
+                        scanEventHandler.onScan(barcode)
+                        barcode = ''
+                    else:
+                        key = SCAN_CODES.get(scancode, NOT_RECOGNIZED_KEY)
+                        barcode = barcode + key
+                    if key == NOT_RECOGNIZED_KEY:
+                        print(f'unknown key, scancode={scancode}')
+
+    except Exception as e:
+        print(f'Exception occurred, will retry. {e}')
+        scanEventHandler.onException(e)
+        raise
+
+
+scan_barcodes_loop()

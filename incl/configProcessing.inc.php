daaaa768049271a9242b8fa03facc1c09c479531
Marc Ole Bulling <Marc-Ole@gmx.de>
2021-09-02 14:29:33 +0200

    Update version

--- incl/configProcessing.inc.php
+++ incl/configProcessing.inc.php
@@ -17,8 +17,8 @@
  */
 
 
-const BB_VERSION          = "1813";
-const BB_VERSION_READABLE = "1.8.1.3";
+const BB_VERSION          = "1814";
+const BB_VERSION_READABLE = "1.8.1.4";
 
 const CONFIG_PATH = __DIR__ . '/../data/config.php';
 const AUTHDB_PATH = __DIR__ . '/../data/users.db';

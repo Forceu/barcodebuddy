dd1bb5b49eae2b1b52e8dde7d380fee5ce637de1
kuhlivisj <gertjan@manenschijn.org>
2021-12-02 19:45:26 +0100

    Ignore MissingParamType

--- incl/configProcessing.inc.php
+++ incl/configProcessing.inc.php
@@ -137,7 +137,7 @@ class GlobalConfig {
 
     /**
      * @param string $input
-     * @param $originalVar
+     * @psalm-suppress MissingParamType
      * @return array|bool|string
      */
     static private function convertCorrectType(string $input, $originalVar) {
